1f12537c0f1c384e36563b5295e483630698ffcf
Hazurl <ricoujules69@gmail.com>
2016-10-03 09:52:58 +0200

    add file

---
+++ Script/Character/Character.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    public Camera cam;
+
+    //FIXME : The damage on a three can't be just an integer
+    public const int CHOP_DAMAGE_PER_SECOND = 20;
+    public const float CHOP_DISTANCE = 20f;
+
+
+    private Transform tf;
+    private Transform camTf;
+    private Vector3 offset;
+
+    void Start () {
+        tf = GetComponent<Transform>();
+        camTf = cam.GetComponent<Transform>();
+        offset = camTf.position - tf.position;
+    }
+
+    void Update () {
+        //Move the Character when press ZQSD
+        //The camera has to follow too
+        InputMovement();
+
+        //Now the character has to look the mouse
+        SetLookRotation();
+
+        //Choping
+        Chop();
+    }
+
+    void InputMovement ()
+    {
+        if (Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            { //Up
+                tf.Translate(Vector3.forward, Space.World);
+            }
+            if (Input.GetKey(KeyCode.D))
+            { //Rigth
+                tf.Translate(Vector3.right, Space.World);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            { //Left
+                tf.Translate(Vector3.left, Space.World);
+            }
+            if (Input.GetKey(KeyCode.S))
+            { //Down
+                tf.Translate(Vector3.back, Space.World);
+            }
+            //Update Camera position
+            camTf.position = tf.position + offset;
+        }
+    }
+
+    void SetLookRotation ()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Quaternion rot = Quaternion.LookRotation(hit.point - tf.position);
+            rot.x = 0; rot.z = 0;
+            tf.rotation = rot;
+        }
+    }
+
+    void Chop ()
+    {
+        if (Input.GetMouseButton(0)) //left button
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(tf.position, transform.TransformDirection(Vector3.forward), out hit, CHOP_DISTANCE))
+            {
+                Debug.DrawRay(tf.position, transform.TransformDirection(Vector3.forward), Color.red);
+                IChopable target = hit.collider.gameObject.GetComponent<IChopable>();
+
+                if (target == null || target.isDead()) return;
+
+                if (target.Damage(CHOP_DAMAGE_PER_SECOND)) Debug.Log("The three is dead !!!");
+            }
+        }
+    }
+}
---
+++ Script/ObjectBehaviour/IChopable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IChopable : MonoBehaviour {
+
+    public int maxLife = 100;
+    private int currentLife;
+
+    private const float TIME_BETWEEN_DAMAGE = 1;
+    private float timeFromLastHit = 0;
+
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+        currentLife = maxLife;
+	}
+
+    void Update ()
+    {
+        text.text = currentLife.ToString();
+        if (timeFromLastHit > 0)
+            timeFromLastHit -= Time.deltaTime;
+    }
+
+    //Currently, this return just a boolean, maybe return a list of item ?
+    public bool Damage (int dam)
+    {
+        if (timeFromLastHit <= 0)
+        {
+            timeFromLastHit = TIME_BETWEEN_DAMAGE;
+            currentLife -= dam;
+        }
+
+        return currentLife <= 0;
+    }
+
+    public bool isDead ()
+    {
+        return currentLife <= 0;
+    }
+}

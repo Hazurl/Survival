92b3510877219b11bff5b3266edb9020c337a9a6
Hazurl <ricoujules69@gmail.com>
2016-10-11 12:23:38 +0200

    Scroll Inventory is ok, when we add other inventry, it work, the sroller is enable only if there is the height is more than the panel

--- Unity_Survival/Assets/Script/Character/Character.cs
+++ Unity_Survival/Assets/Script/Character/Character.cs
@@ -32,6 +32,7 @@ public class Character : MonoBehaviour {
 
         //Initialize inventory, later we should initialize from a save
         inventory = new Inventory( new Inventory.InventorySpace( INVENTORY_CAPACITY_X, INVENTORY_CAPACITY_Y ), "MyInventory", null, true );
+        //inventory = new Inventory( new Inventory.InventorySpace( 4, 4 ), "other", null, true );
     }
 
     void Update () {
